652a854da0a15839dc00978e5a3dcdb8fb1c26c3
Aayushtirmalle <47912269+Aayushtirmalle@users.noreply.github.com>
2019-04-24 11:48:12 +0530

    Add files via upload

---
+++ voice.py
@@ -0,0 +1,49 @@
+import random
+import time
+
+import speech_recognition as sr
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+
+
+
+    # create recognizer and mic instances
+    recognizer = sr.Recognizer()
+    microphone = sr.Microphone()
+
+    # get a random word from the list
+    word = random.choice(WORDS)
+
+    # format the instructions string
+   
